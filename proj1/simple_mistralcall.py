fc3cb3f162b177276315d4dab62046f53f65da8c
memrinal <memrinal@gmail.com>
2025-10-22 22:39:33 -0700

    both llama2 and mistral

--- proj1/simple_mistralcall.py
+++ proj1/simple_mistralcall.py
@@ -5,13 +5,25 @@ import sys
 url = "http://localhost:11434/api/generate"
 prompt = input("\nYou: ").strip()    
     # Prepare the request payload
-data = {
+data_ollama_llama2 = {
+    "model": "llama2",
+    "prompt": prompt,
+    "stream": False  # Get complete response rather than streaming
+    }
+
+response = requests.post(url, json= data_ollama_llama2)
+        
+result = response.json().get('response', 'No response received')
+        
+print("\nllama2:", result)
+
+data_ollama_mistral = {
     "model": "mistral",
     "prompt": prompt,
     "stream": False  # Get complete response rather than streaming
     }
 
-response = requests.post(url, json=data)
+response = requests.post(url, json=data_ollama_mistral)
         
 result = response.json().get('response', 'No response received')
         
